b33a90658d89cdc297fca8d18e0ac0491897690a
Mohamed Yansane <yansane472@gmail.com>
2021-09-15 18:27:11 +0000

    creation des packages controller et models

---
+++ src/main/java/com/big3_2020/fefads/Model/EmployeModel.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.big3_2020.fefads.Model;
+
+/**
+ *
+ * @author user
+ */
+public class EmployeModel {
+    
+}
